ac8cf1514780f233a64273cb347e197e81d85be8
liuguang <fmj_lg@163.com>
2024-10-12 11:55:03 +0800

    优化代码审查功能以支持提交级别的评论
    此更新改进了代码审查机器人的逻辑，现在它能够基于提交时间筛选特定的变更，并对每个提交独立地进行审查和评论。主要变更包括：
    
    - 修改了合并请求的筛选逻辑，从创建时间改为更新时间，以更准确地捕获最近的变更。
    - 引入了提交时间的解析，只对当天的提交进行审查，提高了审查的针对性和效率。- 改进了diff解析，忽略了文件路径中包含'pom.xml'的变更，优化了审查范围。
    - 增加了对每个提交的变更进行单独审查和评论的能力，支持更细致的代码审查。
    - 引入了异常捕获机制，提高了代码的健壮性和错误处理能力。

--- repository/code.py
+++ repository/code.py
@@ -1,7 +1,9 @@
+import logging
 import re
 from datetime import datetime, timedelta
 from config_loader import config
 import gitlab
+
 
 gitlab_config = config['gitlab']
 
@@ -12,7 +14,7 @@ gl.auth()
 # 获取当天的时间范围
 today_start = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
 today_end = today_start + timedelta(days=1)
-
+today = datetime.now().date()
 # 格式化时间字符串，GitLab API 需要 ISO 8601 格式的时间
 start_date = today_start.isoformat()
 end_date = today_end.isoformat()
@@ -24,61 +26,81 @@ def get_mrs_created_today() -> list:
     # 遍历所有项目
     for project in gl.projects.list(all=True):
         # 获取项目中的所有合并请求
-        mrs = project.mergerequests.list(state='opened', created_after=start_date, created_before=end_date, all=True)
+        mrs = project.mergerequests.list(state='opened', updated_after=start_date, updated_before=end_date)
+        # mrs = project.mergerequests.list(state='opened')
         all_mrs.extend(mrs)
     return all_mrs
 
 
 def parse_diffs(all_mrs, callback=None) -> None:
+    print(all_mrs)
     for item in all_mrs:
         project = gl.projects.get(item.project_id)
         mr = project.mergerequests.get(item.iid)
-        # 获取合并请求的更改详情
-        changes = mr.changes()
-        # 获取合并请求的更改详情
-        for file_change in changes['changes']:
-            file_path = file_change['new_path'] if 'new_path' in file_change else file_change['old_path']
-            if file_change['deleted_file']:
-                continue
-            diff = file_change['diff']
-            pattern = re.compile(r'@@ -\d+,\d+ \+(\d+),(\d+) @@')
-            matches = pattern.findall(diff)
-            diff_list = list(filter(None, diff.split('@@ -')))
-            for index, item_diff in enumerate(diff_list):
-                clear_diff_line = clear_diff(item_diff)
-                if not clear_diff_line:
-                    continue
-                body = callback(item_diff)
-                if not body:
-                    continue
-                print(clear_diff_line)
-                print(file_path)
-                line = matches[index]
-                start_line = int(line[0])
-                end_line = int(line[1]) + start_line - 1
-                body = f"### **文件：{file_path}" + f"第{start_line}到{end_line}行**\n" + f"\nAI review:{body}"
-                discussion = mr.notes.create({
-                    'body': body,
-                    'position': {
-                        'base_sha': mr.diff_refs.get('base_sha', ""),
-                        'head_sha': mr.diff_refs.get('head_sha', ''),
-                        'start_sha': mr.diff_refs.get('start_sha', ""),
-                        'position_type': 'text',
-                        'new_line': True,
-                        # 'new_start_line': start_line,
-                        # 'new_end_line': end_line,
-                        "file_path": file_path,
-                        'line': start_line,
-                        'line_type': 'new'
-                    },
-                    'resolve': False
-                })
-                discussion.save()
+        # 获取合并请求的提交记录
+        commits = mr.commits()
+        for commit in commits:
+            commit_date = datetime.strptime(commit.committed_date, "%Y-%m-%dT%H:%M:%S.%fZ").date()
+            if commit_date == today:
+                diffs = commit.diff(get_all=True, deleted_file=False, diff=True)
+                print(f"today diffs: {diffs}")
+                for file_change in diffs:
+                    file_path = file_change['new_path'] if 'new_path' in file_change else file_change['old_path']
+                    if file_path.find('pom.xml') >= 0:
+                        continue
+                    diff = file_change['diff']
+                    pattern = re.compile(r'@@ -\d+,\d+ \+(\d+),(\d+) @@')
+                    matches = pattern.findall(diff)
+                    diff_list = list(filter(None, diff.split('@@ -')))
+                    for index, item_diff in enumerate(diff_list):
+                        try:
+                            clear_diff_line = clear_diff(item_diff)
+                            if clear_diff_line is None:
+                                continue
+                            
+                            body = callback(item_diff)
+                            if body is None:
+                                continue
+                            
+                            line = matches[index]
+                            if len(line) == 0:
+                                continue
+                            
+                            start_line = int(line[0])
+                            end_line = int(line[1]) + start_line - 1
+                            
+                            body = (
+                                f"### **文件：{file_path} 第{start_line}到{end_line}行**\n"
+                                f"\nAI review:{body}"
+                            )
+                            
+                            discussion = mr.notes.create({
+                                'body': body,
+                                'position': {
+                                    'base_sha': mr.diff_refs.get('base_sha', ""),
+                                    'head_sha': mr.diff_refs.get('head_sha', ''),
+                                    'start_sha': mr.diff_refs.get('start_sha', ""),
+                                    'position_type': 'text',
+                                    'new_line': True,
+                                    "file_path": file_path,
+                                    'line': start_line,
+                                    'line_type': 'new'
+                                },
+                                'resolve': False
+                            })
+                            discussion.save()
+                        
+                        except Exception as e:
+                            # 更具体的异常捕获可以在这里添加
+                            print(f"An error occurred: {e}")
+                            # 可以考虑记录详细的错误日志
+                            # logging.error(f"An error occurred: {e}", exc_info=True)
 
 
 def clear_diff(diff) -> str:  # 清除diff中的无用信息
     diff_list = diff.split('\n')
-    diff_list = [line for line in diff_list if not (line.strip().startswith('-') or line.strip() == '')]
+    diff_list = [line for line in diff_list
+                 if not (line.strip().startswith('-') or line.strip() == '' or line.find('import') >= 0)]
     if len(diff_list) > 0:
         diff = '\n'.join(diff_list)
         if contains_letters(diff):
